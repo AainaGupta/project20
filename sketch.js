250ff820df9d84d0f16f58f51473fd414a8c30ce
AainaGupta <75073482+AainaGupta@users.noreply.github.com>
2020-12-10 17:35:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,11 +14,11 @@ function draw() {
   background(255,255,255);  
   if(wall.x-car.x<car.width/2+wall.width/2){
     car.velocityX=0;
-    var deformation=0.5*weight*speed*speed/22509;
+    var deformation=0.5 * weight * speed * speed/22509;
     if(deformation>180){
       car.shapeColor="red";
     }
-    if(deformation<180){
+    if(deformation<180 && deformation>100){
       car.shapeColor="yellow";
 
     }
@@ -26,7 +26,6 @@ function draw() {
       car.shapeColor="green";
     }
   }
-  else
-  car.shapeColor=color(0,250,0);
+ 
   drawSprites();
 }
